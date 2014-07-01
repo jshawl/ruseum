83aebd51a6633e8605d9f8c6d1a87a11aaa31261
Jesse Shawl <jesse@jshawl.com>
2014-07-01 09:51:28 -0400

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  resources :museums
+  resources :museums do
+    resources :paintings
+  end
 
-  resources :paintings
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
